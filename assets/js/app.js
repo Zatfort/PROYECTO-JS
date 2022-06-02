08046d7aac0e4c65e99dbcf7f1cda64e8c2c5caa
daniel gutierrez <daniel_gutierrezn15@hotmail.com>
2022-06-02 20:46:11 -0300

    js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,143 @@
+
+class Libroso {
+    constructor(nombre, tapa, precio) {
+        this.nombre = nombre;
+        this.tapa = tapa;
+        this.precio = precio;
+    }
+};
+
+const libros1 = new Libroso('Mafalda', "Hardbook", 5.076)
+const libros2 = new Libroso('Berserk Volume 1', "Paperbook", 2.221, 91)
+const libros3 = new Libroso('World of Warcraft', "blanbook", 1.908, 14)
+
+
+const carrito = [];  
+const mensaje = (libroElegido) => {
+    switch (libroElegido) {
+        case "mafalda":
+            console.log(libros1.nombre `Agregado al carrito`);
+            break;
+        case "berserk":
+            console.log(libros2.nombre `Agregado al carrito`);
+            break;
+        case "world of warcraft":
+            console.log(libros3.nombre `Agregado al carrito`);
+            break;
+        default:
+            break;
+    }
+}; //---> SOLO CUMPLE LA FUNCION DE MOSTRAR UN MENSAJE QUE DICE QUE FUE AGREGADO "X" PEDAL AL CARRITO
+
+const totalCarrito = () => {
+    let sumaTotal = 0;
+    carrito.forEach((producto) => {
+        sumaTotal += producto.precio;
+    });
+    return sumaTotal;
+};
+
+
+
+
+
+const libros = [
+    libroMafalda,
+    libroBerserk,
+    libroWorldofwarcraft,
+];
+
+
+
+function agregarlibro() {
+    const libroElegido = prompt(`Elija su libro: mafalda berserk world of warcraft `);
+    if (libroElegido != null) {
+        mensaje(libroElegido);
+
+        switch (libroElegido) {
+            case "mafalda":
+                carrito.push(libroMafalda);
+                break;
+            case "berserk":
+                carrito.push(libroBerserk);
+                break;
+            case "world of warcraft":
+                carrito.push(libroWorldofwarcraft);
+                break;
+            default:
+                alert("ASEGURESE DE HABER ESCRITO CORRECTAMENTE EL NOMBRE DEL LIBRO");
+                break;
+
+        }
+
+
+
+        let continuar = confirm("¿DESEA AGREGAR OTRO LIBRO A SU COMPRA?");
+        if (continuar) {
+            agregarlibro();
+        } else {
+            console.log(`Total a abonar $${totalCarrito()}`);
+        }
+
+    } else {
+        let buscar = prompt('si no sabes que libro elegir ,puedes buscar por su tipo de tapa (prueba con "hardbook"-"paperbook")');
+
+        if (buscar != null) {
+            const buscarTapa = libros.filter((libroso) => libroso.tapa.includes(buscar));
+
+            if (buscarTapa.length > 1) {
+                if (confirm(`Te podria interesar nuestro ${(buscarTapa[0].nombre).toUpperCase()} o nuestro ${(buscarTapa[1].nombre).toUpperCase()} agregralo al carrito`)) {
+                    agregarlibro();
+                } else {
+                    alert("gracias por su visita");
+                }
+            } else {
+                if ((confirm(`Te podria interesar nuestro ${(buscarTapa[0].nombre).toUpperCase()} agregalo al carrito `))) {
+                    agregarlibro();
+                } else {
+                    alert("gracias por su visita");
+                }
+            }
+        } else {
+            alert("gracias por su visita");
+        }
+
+    };
+}
+
+agregarlibro();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
 // Simulador de Carrito
 
 class Libros {
@@ -27,6 +167,16 @@ const totalCarrito = () => {
     })
     return sumaTotal
 }
+
+// objectos para la barra de busqueda 
+const librosop = [
+    {nombre: "Mafalda"},
+    {nombre: "berserk"},
+    {nombre: "world of warcraft "},
+]
+
+
+
 
 alert("bienvenidos a Paper Factory Sale")
 
@@ -64,3 +214,4 @@ const agregarProducto = () => {
 agregarProducto()
 
 alert ("Muchas Gracias por su Compra")
+*/
